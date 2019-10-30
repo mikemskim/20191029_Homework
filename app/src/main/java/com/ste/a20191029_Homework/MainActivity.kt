91f189bda0ac9877c394b648af21300cfc797118
mikemskim <mskim@steconsulting.co.kr>
2019-10-30 22:07:35 +0900

    로그인 버튼 이벤트 추가 - admin 로그인시 토스트 메시지 출력 - 다른 경우 id / pwd 출력

--- app/src/main/java/com/ste/a20191029_Homework/MainActivity.kt
+++ app/src/main/java/com/ste/a20191029_Homework/MainActivity.kt
@@ -2,11 +2,23 @@ package com.ste.a20191029_Homework
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loginBtn.setOnClickListener {
+            var id = loginId.text.toString()
+            var pwd = password.text.toString()
+            var message = "ID : $id, PWD : $pwd"
+            if (id == "admin" && pwd == "1234") {
+                message = "관리자로 로그인하였습니다."
+            }
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        }
     }
 }
