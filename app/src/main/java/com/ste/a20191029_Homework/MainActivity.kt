b691901be8885264106e6dc8df734e870942282a
mskim@steconsulting.co.kr <kim40100!!>
2019-10-30 15:30:39 +0900

    완성

---
+++ app/src/main/java/com/ste/a20191029_Homework/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ste.a20191029_Homework
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
